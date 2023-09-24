46bfc1aab29492d387080d0b61fb0fef290c71a4
Rintia Islam <rintiaislam110@gmail.com>
2023-09-24 23:41:27 +0600

    First Root route added

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,10 @@
+
+const Root = () => {
+    return (
+        <div>
+            <h1>This is root</h1>
+        </div>
+    );
+};
+
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Root from './Components/Root/Root';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
