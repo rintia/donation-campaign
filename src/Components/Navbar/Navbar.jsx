4ec230b8fe5fca1e49ecd55d055ccecba2862ba3
Rintia Islam <rintiaislam110@gmail.com>
2023-09-25 00:47:28 +0600

    active link style added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,32 @@
 import { NavLink } from "react-router-dom";
-
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 mt-8">
+        <nav>
+            <div className="navbar bg-base-100 mt-8">
         <div className="flex-1">
           <img className="lg:w-2/12 md:w-3/12" src="../../../public/Logo.png" alt="" />
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
-            <li> <NavLink to='/' >Home</NavLink> </li>
-            <li> <NavLink to='/donation' >Donation</NavLink> </li>
-            <li> <NavLink to='/statistic' >Statistic</NavLink> </li>
+            <li> <NavLink style={({ isActive }) => ({
+            color: isActive ? '#FF444A' : '#0B0B0B',
+            background: '#FFFFFFF2',
+            })}
+             to='/' >Home</NavLink> </li>
+            <li> <NavLink  style={({ isActive }) => ({
+            color: isActive ? '#FF444A' : '#0B0B0B',
+            background: '#FFFFFFF2',
+        })} 
+            to='/donation' >Donation</NavLink> </li>
+            <li> <NavLink   style={({ isActive }) => ({
+            color: isActive ? '#FF444A' : '#0B0B0B',
+            background: '#FFFFFFF2',
+        })}
+            to='/statistic' >Statistic</NavLink> </li>
           </ul>
         </div>
       </div>
+        </nav>
     );
 };
 
