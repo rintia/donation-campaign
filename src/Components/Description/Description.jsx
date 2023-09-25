603def945503b1c0af029584fd3557625dfd5486
Rintia Islam <rintiaislam110@gmail.com>
2023-09-25 15:10:54 +0600

    donated id are saved in localstorage

--- src/Components/Description/Description.jsx
+++ src/Components/Description/Description.jsx
@@ -1,9 +1,11 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
+import { saveDonation } from "../../localStorage";
 
 const Description = () => {
     const handleDonate =() =>{
+        saveDonation(idInt)
         toast('Successfully Donated')
     }
     const cardDetails = useLoaderData();
