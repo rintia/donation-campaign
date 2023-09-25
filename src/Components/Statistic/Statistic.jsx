c9b72662002843b94141307a0c106746348f23ce
Rintia Islam <rintiaislam110@gmail.com>
2023-09-25 19:56:46 +0600

    Pie chart added to satistics route

--- src/Components/Statistic/Statistic.jsx
+++ src/Components/Statistic/Statistic.jsx
@@ -1,10 +1,57 @@
 
+import {  Pie, PieChart, Cell, Legend } from 'recharts';
+import { getStoredDonation } from '../../localStorage';
+
+
 const Statistic = () => {
+      const myDonations = getStoredDonation();
+      const donationLength = myDonations.length ;
+      const remainingDonation = 12 - donationLength;
+      const data = [
+        { name: 'Total Donation', value: remainingDonation },
+        { name: 'Your Donation', value: donationLength }
+      ];
+
+      const COLORS = ['#FF444A', '#00C49F'];
+
+      const RADIAN = Math.PI / 180;
+      const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+      
+        return (
+          <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+          </text>
+        );
+      };
+      
     return (
-        <div>
-            <h1>This is Statistic</h1>
+        <div className='flex justify-center mt-8'>
+            
+            <PieChart width={600} height={500}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={180}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Legend></Legend>
+        </PieChart>
+
+            
         </div>
     );
 };
+
 
 export default Statistic;
