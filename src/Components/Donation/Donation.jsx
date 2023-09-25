9ceff1f7d0978776b0bd28d974afc06773e8add7
Rintia Islam <rintiaislam110@gmail.com>
2023-09-25 16:26:59 +0600

    see All feautres added to donation route

--- src/Components/Donation/Donation.jsx
+++ src/Components/Donation/Donation.jsx
@@ -6,6 +6,7 @@ import MyDonation from "../MyDonation/MyDonation";
 const Donation = () => {
     const allData = useLoaderData();
     const [myDonations, setMyDonations] = useState([]);
+    const [dataLength, setDataLength] =useState(4);
     useEffect( () => {
         const storedDonation = getStoredDonation();
         if(allData.length > 0){
@@ -20,13 +21,18 @@ const Donation = () => {
         }
     },[])
     return (
-        <div className="mt-16">
+        <div className="my-16 lg:px-0 px-4">
            
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
                 {
-                    myDonations.map(myDonation => <MyDonation key={myDonation.id} myDonation={myDonation}></MyDonation>)
+                    myDonations.slice(0, dataLength).map(myDonation => <MyDonation key={myDonation.id} myDonation={myDonation}></MyDonation>)
                 }
             </div>
+           <div className={dataLength == myDonations.length && 'hidden'}>
+           <div className='flex justify-center mt-8'>
+                <button onClick={() => setDataLength(myDonations.length)} className="btn bg-[#009444] text-white">See All</button>
+            </div>
+           </div>
         </div>
     );
 };
--- src/Components/MyDonation/MyDonation.jsx
+++ src/Components/MyDonation/MyDonation.jsx
@@ -3,7 +3,7 @@ const MyDonation = ({myDonation}) => {
     const{title, category, image, text_color, category_color, card_color, price} = myDonation;
     return (
         <div className="card card-side lg:h-48 shadow-xl" style={{backgroundColor: `${card_color}`}} >
-        <figure><img className='h-full' src={image} alt="Movie"/></figure>
+        <figure className='w-2/5'><img className='h-full w-full' src={image} alt="Movie"/></figure>
         <div className="card-body">
             <h1 style={{color:`${text_color}`}} className='text-sm'><span style={{backgroundColor: `${category_color}`}} className='px-2 rounded-xl'>{category}</span></h1>
           <h2 className="card-title font-bold">{title}</h2>
